3353e7de287ae93909aaca88857920099c393482
LifeDeath247 <ysrathod6789@gmail.com>
2024-07-17 11:01:17 +0530

    new

---
+++ max_consecutive_ones_in_binary_no.py
@@ -0,0 +1,10 @@
+def consecutive(n):
+    str = bin(n)[2:]
+    print('Binary conversion of ', n, 'is: ', str)
+    result = max(len(group) for group in str.split('0'))
+    return result
+
+if __name__ == '__main__':
+    n = int(input("Enter the value of n: ").strip())
+    max_ones = consecutive(n)
+    print('Maximun number of consecutive 1 are: ',max_ones)
